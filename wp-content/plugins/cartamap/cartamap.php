f0a43c8ae9d1d0a5f9064420a4568f38236a079b
Stephen Gralton <stephen.gralton@gmail.com>
2017-03-22 18:18:23 -0600

    Add files via upload
    Updated commented-out section to include correct text-domain for i18n

--- wp-content/plugins/cartamap/cartamap.php
+++ wp-content/plugins/cartamap/cartamap.php
@@ -5,15 +5,12 @@
  * from Mexico  City to north of Santa Fe NM
  * This particular file is responsible for including the necessary dependencies and starting the plugin.
  * Plugin Name:       Carta Interactive Map
- * Plugin URI:        http://example.com
  * Description:       Display an interactive map of the Camino Real
  * Version:           0.2.0
  * Author:            CNM Capstone students: Spring 2017
- * Author URI:        http://example.com
- * Text Domain:       carta-locale
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain: cartamap-locale
+ * Text Domain: 	  carta-locale
  * Domain Path:       /languages
  */
  // Domain path is used to internationalize the plugin
